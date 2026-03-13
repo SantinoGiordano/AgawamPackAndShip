0c2eb51cdd3cc04f2f5e2f130b02080d78482523
SantinoGiordano <santinogiordano13@gmail.com>
2026-03-13 16:51:03 -0400

    inital header/ hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,33 @@
-export default function Home() {
-  return(
-    <div>
-      <h1>Welcome to Agawam Pack and Ship!</h1>
-    </div>
-  )
+export default function Hero() {
+  return (
+
+   <>
+    <section className="w-full bg-sky-200 py-16">
+      <div className="max-w-6xl mx-auto flex items-center justify-between px-8">
+        
+        {/* Left Text */}
+        <div className="flex flex-col">
+          <h1 className="text-6xl font-bold text-purple-800">
+            Agawam
+          </h1>
+
+          <h1 className="text-3xl font-semibold text-amber-700 mt-2">
+            Pack and Ship
+          </h1>
+        </div>
+
+        {/* Right Image */}
+        <div className="flex-shrink-0">
+          <img
+            src="/frontOfStore.jpg"
+            alt="Front of Store"
+            className="w-[420px] rounded-xl shadow-lg"
+            />
+        </div>
+
+      </div>
+    </section>
+  </> 
+    
+  );
 }
